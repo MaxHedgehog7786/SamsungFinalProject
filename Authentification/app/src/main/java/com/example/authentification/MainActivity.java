ceeb0424b0b6de6d925f0b3ba02c4b0c4b736247
Maxim Mihalishin <maxim.mihalishin@yandex.ru>
2024-04-15 17:25:53 +0300

    V1.0

---
+++ Authentification/app/src/main/java/com/example/authentification/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.authentification;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+    EditText emailText, passwordText;
+    Button btn;
+    FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        emailText = findViewById(R.id.email);
+        passwordText = findViewById(R.id.password);
+        btn = findViewById(R.id.btn);
+        mAuth = FirebaseAuth.getInstance();
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailText.getText().toString();
+                String password = passwordText.getText().toString();
+
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Пользователь успешно зарегистрирован
+                                    Log.d("TAG", "createUserWithEmail:success");
+                                    Toast.makeText(MainActivity.this, "Authentication success.",
+                                            Toast.LENGTH_SHORT).show();
+                                } else {
+                                    // Регистрация не удалась
+                                    Log.w("TAG", "createUserWithEmail:failure", task.getException());
+                                    Toast.makeText(MainActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+    }
+}
